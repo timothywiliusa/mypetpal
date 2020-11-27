c1ad6efaec3d0697221d48e5c7e380cde3b1c073
Timothy <wiliusat@oregonstate.edu>
2020-11-27 09:50:27 -0800

    sign in handle submit for non-google login

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input-component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
 	constructor(props) {
@@ -16,10 +16,16 @@ class SignIn extends Component {
 		};
 	}
 
-	handleSubmit = (e) => {
-		e.preventDefault();
+	handleSubmit = async (event) => {
+		event.preventDefault();
 
-		this.setState({ email: '', password: '' });
+		const { email, password } = this.state;
+		try{
+			await auth.signInWithEmailAndPassword(email,password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	handleChange = (e) => {
