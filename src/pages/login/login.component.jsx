82aadfce99e8fc4e93ecb7339fb5d4e0397b8c56
Peyton Frith <frithp@oregonstate.edu>
2021-01-31 14:52:26 -0800

    Revert "Merge pull request #5 from timothywiliusa/peyton"
    This reverts commit b47fa7431fa7457f46d93a1cdb37f721b40b8734, reversing
    changes made to e42ace4be391a1f21ea5316e786e67ce15a8a76f.

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './login.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const login = () => (
 	<div className="login">
