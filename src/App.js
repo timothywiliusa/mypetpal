96ce0cf49ad02cadd19cb86e20d5e91a5108ad86
peytonfrith <frithp@oregonstate.edu>
2021-02-02 14:17:48 -0800

    Revert "Merge pull request #8 from timothywiliusa/eric"
    This reverts commit 60a095c893024911792ccb6f260e3cbe40a181e5, reversing
    changes made to e6003c95b55344b28167c7904200e780c6ba0903.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Friends from './pages/friends/friends.component';
 import Vets from './pages/vets/vets.component';
 import Dashboard from './pages/dashboard/dashboard.component';
 import Shop from './pages/shop/shop.component';
-import UserProfile from './pages/userprofile/userprofile.component';
 
 
 class App extends Component {
@@ -64,7 +63,6 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/userprofile" component={UserProfile}/>
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/support" component={Support} />
           <Route exact path="/pets" component={Pets} />
