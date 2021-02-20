f632c94c923e4a54f16b331edb7433cbcc115840
dongyisun <sundo@oregonstate.edu>
2021-02-20 00:19:51 -0800

    user infor page complete

---
+++ src/pages/userprofile/userprofile.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './userprofile.styles.scss';
+import Userprofile from '../../components/displayuser-info/displayuser-info.component';
+
+const userprofile = () => (
+  <div className = "displayuser-info">
+    <Userprofile/>
+  </div>
+);
+
+export default userprofile;
